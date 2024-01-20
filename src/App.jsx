f15100018d6e301c750cd0927143082f5be28f7d
Po Lien Lin <polien.joe.lin@gmail.com>
2024-01-20 14:09:02 +0800

    Set Initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 function App() {
-  return <div>here is App</div>;
+  return (
+    <form className="search-form max-w-[400px] my-[50px] mx-auto">
+      <input type="text" className="search" placeholder="City or State" />
+      <ul className="suggestions">
+        <li>Filter for a city</li>
+        <li>or a state</li>
+      </ul>
+    </form>
+  );
 }
 
 export default App;
