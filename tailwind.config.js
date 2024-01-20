bb53bf7740d674fd3c533db3c903d7db22a66e21
Po Lien Lin <polien.joe.lin@gmail.com>
2024-01-20 15:41:11 +0800

    Revert "Feature/fetching data"

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      boxShadow: {
-        input: "0 0 5px rgba(0, 0, 0, 0.12), inset 0 0 2px rgba(0, 0, 0, 0.19)",
-        suggestions: "0 0 10px rgba(0, 0, 0, 0.14);",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
