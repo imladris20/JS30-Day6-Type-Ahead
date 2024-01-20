6403e8bc1391929dbc34edee3588c59cd8f0f28d
Po Lien Lin <polien.joe.lin@gmail.com>
2024-01-20 15:40:05 +0800

    Accomplish fetching data to change cities

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [cities, setCities] = useState([]);
+
+  useEffect(() => {
+    const endPoint =
+      "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+    const fetchData = async () => {
+      try {
+        const data = await axios.get(endPoint);
+        setCities(data.data);
+      } catch (error) {
+        console.error("Error Occured: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(cities);
+
   return (
     <form className="search-form max-w-[400px] my-[50px] mx-auto">
       <input
