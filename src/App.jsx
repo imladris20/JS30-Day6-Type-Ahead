13992cab8d19902dc9b611187939b1cc3ec2bc23
Po Lien Lin <polien.joe.lin@gmail.com>
2024-01-21 21:40:18 +0800

    Fetch data from my own firebase realtime database

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import InitialListItems from "./Components/ListItems/InitialListItems";
 import useDataLoader from "./Hooks/useDataLoader";
 
 const endPoint =
-  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+  "https://cities-97712-default-rtdb.asia-southeast1.firebasedatabase.app/cities.json";
 
 function App() {
   const cities = useDataLoader(endPoint);
