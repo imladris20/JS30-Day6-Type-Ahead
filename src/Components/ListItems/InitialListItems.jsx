5259962e904b4cdec340f5242ff5edd09a19fc6c
Po Lien Lin <polien.joe.lin@gmail.com>
2024-01-21 15:10:04 +0800

    Change font family of list items

--- src/Components/ListItems/InitialListItems.jsx
+++ src/Components/ListItems/InitialListItems.jsx
@@ -4,7 +4,7 @@ const InitialListItems = () => {
   return initialMessages.map((message, index) => {
     return (
       <li
-        className="bg-white border-b-[1px] border-solid border-[#d8d8d8] p-5 flex justify-between items-center capitalize transition-colors duration-200 even:bg-gradient-to-b odd:bg-gradient-to-t from-white to-[#efefef] shadow-suggestions"
+        className="bg-white border-b-[1px] border-solid border-[#d8d8d8] font-serif p-5 flex justify-between items-center capitalize transition-colors duration-200 even:bg-gradient-to-b odd:bg-gradient-to-t from-white to-[#efefef] shadow-suggestions"
         key={index}
       >
         {message}
